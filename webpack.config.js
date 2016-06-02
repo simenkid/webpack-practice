e5b91390191760e4308f8addd1293d2bff3bd88e
Simen Li <simenkid@gmail.com>
2016-06-02 23:09:29 +0800

    step05 enable sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -28,12 +28,14 @@ switch (process.env.npm_lifecycle_event) {
     case 'build':
         config = merge(
             common,
+            { devtool: 'source-map '},
             parts.setupCSS(PATHS.app)
         );
         break;
     default:
         config = merge(
             common,
+            { devtool: 'eval-source-map '},
             parts.setupCSS(PATHS.app),
             parts.devServer({
                 host: process.env.HOST,
