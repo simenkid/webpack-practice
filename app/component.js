a375b7791b68bbac37eabf7ee32a7cac3cd84db5
Simen Li <simenkid@gmail.com>
2016-06-03 21:43:26 +0800

    step07 css local scoping test

--- app/component.js
+++ app/component.js
@@ -1,5 +1,8 @@
+var styles = require('./main.css');
+
 module.exports = function () {
     var element = document.createElement('h1');
+    element.className = styles.redButton;
     element.innerHTML = "Hello World";
     return element;
 };
