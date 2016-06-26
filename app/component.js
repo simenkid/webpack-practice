3fb2658e339cbc3240e7d80ee936d72e253bf551
Simen Li <simenkid@gmail.com>
2016-06-26 22:24:07 +0800

    ok config

--- app/component.js
+++ app/component.js
@@ -1,9 +1,6 @@
-var styles = require('./a.css'),
-    styles2 = require('./b.css');
-
 module.exports = function () {
     var element = document.createElement('h1');
-    element.className = styles.redButton + ' ' + styles2.deco + ' ' + 'pure-button';
+    // element.className = styles.redButton + ' ' + styles2.deco + ' ' + 'pure-button';
     //element.className = 'pure-button';
     element.innerHTML = "Hello World";
     return element;
